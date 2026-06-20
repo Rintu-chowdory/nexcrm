64b1a08ee37844a63590ebfa1ef28717d9a686f2
Rintu-chowdory <chowdoryrintu492@gmail.com>
2026-06-20 23:20:12 +0200

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0d1117',
+        'navy-light': '#161b22',
+        'blue-accent': '#3b82f6',
+      }
+    },
+  },
+  plugins: [],
+}
